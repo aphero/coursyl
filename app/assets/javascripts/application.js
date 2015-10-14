34622f6fb049aff369188b8a950e4cd3289fa344
Josh McKelvey <jmckelvey0@gmail.com>
2015-10-13 20:56:04 -0400

    add modal functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,5 +69,28 @@ function deleteSelect() {
 
 $(function() {
   $(".btn-danger").on("click", deleteSelect);
-  return false;
 });
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+function popModal() {
+  $('#super_modal').modal('show')
+}
+
+
+$(function() {
+  $(".fa.fa-calendar").on("click", popModal)
+})
